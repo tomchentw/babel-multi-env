9a5511fe023d008ebcb2dd5206eb815d39ce1028
Tom Chen <developer@tomchentw.com>
2017-10-27 12:13:45 +0800

    test(bin): fix CLI changes

--- bin/__tests__/babel-multi-env.spec.js
+++ bin/__tests__/babel-multi-env.spec.js
@@ -4,7 +4,7 @@ import { execSync } from "child_process";
 describe("babel-multi-env bin", () => {
   it("should compile runnable script on the platform", () => {
     execSync(
-      "node bin/babel-multi-env.js --multi-versions 8.0.0,6.0.0,4.0.0,0.12.0,0.10.0 --out-dir tmp bin/__fixtures__/arrayJoin.fixture.js"
+      "node bin/babel-multi-env.js --multi-versions 8.0.0 6.0.0 4.0.0 0.12.0 0.10.0 --given bin/__fixtures__/arrayJoin.fixture.js --out-dir tmp"
     );
     const result = execSync("node tmp/arrayJoin.fixture.js");
     expect(result.toString("utf8")).toMatchSnapshot();
